561dde0599c795df245ecbf818377733ad9586fa
Christina <madmadchen@gmail.com>
2016-11-25 13:52:17 -0500

    Rejigger menu links so that both the link and the +/- will trigger menu animations.

--- wp-content/plugins/wp-parsi-navigation-trees/js/wp-parsi-navigation-trees.js
+++ wp-content/plugins/wp-parsi-navigation-trees/js/wp-parsi-navigation-trees.js
@@ -1,30 +1,25 @@
 jQuery(document).ready(function(){
 
-  jQuery('#wb_tree ul li').has('.sub-menu').prepend('<span>+</span>');
+  jQuery('#wb_tree ul li').has('.sub-menu')
+    .children('a')
+    .addClass('toggle_menu')
+    .prepend('<span>+</span>');
+    
   const location = window.location.href.replace(/\/$/, '');
   jQuery('#wb_tree ul li a[href="' + location + '"').siblings('ul').show();
   jQuery('#wb_tree ul li a[href="' + location + '"').siblings('span').text('-');
-  jQuery('#wb_tree ul .sub-menu').prepend('<div id="pacman"></div>');
 
-  jQuery('#wb_tree span').click(
-    function(){}).toggle(
-       function(){
-           // Sorry about the below for anyone who might see it and know better, but it works.
-           jQuery('#wb_tree >div >ul >li >span').text('+')
-           jQuery(this).text('-');
-           
-           jQuery('#wb_tree ul .sub-menu').hide();
-       	   var parent = jQuery(this).parent();
-           jQuery(parent).find('>ul').fadeIn();
-       },
-       function(){
-           jQuery(this).text('+');
-           var parent = jQuery(this).parent();
-           jQuery(parent).find('>ul').fadeOut();
-       }
-    )
-    
-    jQuery("li:contains('+')").css("background-image", "none");
+  jQuery('#wb_tree a.toggle_menu').click(function(){}).toggle(open,close)});
 
+const open = function(){
+  jQuery('#wb_tree ul li span').text('+')
+  jQuery(this).find('span').text('-');
+  
+  jQuery('#wb_tree ul .sub-menu').hide();
+  jQuery(this).siblings('ul').fadeIn();
+}
 
-});
+const close = function(){
+  jQuery(this).find('span').text('+');
+  jQuery(this).siblings('ul').fadeOut();
+}
