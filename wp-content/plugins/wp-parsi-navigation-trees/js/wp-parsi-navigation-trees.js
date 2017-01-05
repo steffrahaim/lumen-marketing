d74f32e262ff76aea14e8c64bb8058ea0fc44194
Steff Rahaim <steffrahaim@Steffs-MacBook-Pro.local>
2017-01-04 21:24:56 -0500

    NextGen Galleries. PDF Embeds.

--- wp-content/plugins/wp-parsi-navigation-trees/js/wp-parsi-navigation-trees.js
+++ wp-content/plugins/wp-parsi-navigation-trees/js/wp-parsi-navigation-trees.js
@@ -134,7 +134,6 @@ function open(element, animate=true, delayLoad=false) {
   //}
 }
 
-
 function queueChildren(children, delayLoad=false) {
   const duration = 400;
   // STEFFERS! Change the below from duration to a value to tweak.
